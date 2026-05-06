6e165e3dc31631dce6eb01a340491f764bcbe094
slandarer <1410814635@qq.com>
2026-05-06 15:16:56 +0800

    add & push by Matlab

---
+++ README.md
@@ -0,0 +1,35 @@
+# roseball
+Draw a roseball using MATLAB.
+
+
+
+```matlab
+roseball()
+```
+![](roseball/_demo_b1.png)
+
+```matlab
+CList = [.21 .09 .38; .29 .07 .47; .40 .11 .49; .55 .16 .51;
+         .75 .24 .47; .89 .32 .41; .97 .49 .37; 1.0 .56 .41;
+         1.0 .69 .49; 1.0 .82 .59; .99 .92 .67; .98 .95 .71];
+roseball(gca, CList)
+```
+![](roseball/_demo_o1.png)
+```matlab
+CList = [.13 .10 .16; .20 .09 .20; .28 .08 .23; .42 .08 .30;
+         .51 .07 .34; .66 .12 .35; .79 .22 .40; .88 .35 .47;
+         .90 .45 .54; .89 .78 .79];
+roseball(CList)
+```
+![](roseball/_demo_r1.png)
+
+## Schematic Diagram
+
+![](Schematic_Diagram.png)
+
+## 🎨 Inspiration
+It is worth mentioning that this rose ball was inspired by an origami flower ball I made when I was very young. 
+![](roseball.png)
+
+Other origami flower balls can be found here:
+![](origami.jpg)
---
+++ roseball/roseball.m
@@ -0,0 +1,56 @@
+function roseball(varargin)
+% Zhaoxu Liu / slandarer (2026). roseball 
+% (https://www.mathworks.com/matlabcentral/fileexchange/93510-roseball), 
+% MATLAB Central File Exchange. Retrieved April 15, 2026.
+ax = gca; CList = [.02 .04 .39; .02 .06 .69; .01 .26 .99; .17 .69 1];
+if nargin >= 1 && isa(varargin{1}, 'matlab.graphics.axis.Axes')
+    ax = varargin{1};
+    if nargin >= 2
+        CList = varargin{2};
+    end
+elseif nargin >= 1
+    CList = varargin{1};
+    if nargin >= 2
+        ax = varargin{2};
+    end
+end
+hold(ax, 'on'); view(11, -.07); axis equal off
+
+% Basic surface
+[x, t] = meshgrid((0:24)./24, (0:.5:575)./575.*20.*pi + 4*pi);
+p = (pi/2)*exp(-t./(8*pi));
+u = 1 - (1 - mod(3.6*t, 2*pi)./pi).^4./2 + sin(15*t)/150;
+y = 2*(x.^2 - x).^2.*sin(p);
+r = u.*(x.*sin(p) + y.*cos(p));
+h = u.*(x.*cos(p) - y.*sin(p)) + .35;
+x = r.*cos(t); y = r.*sin(t);
+
+% Rotation matrix
+Rx = @(rx) [1, 0, 0; 0, cos(rx), -sin(rx); 0, sin(rx), cos(rx)];
+Rz = @(yz) [cos(yz), - sin(yz), 0; sin(yz), cos(yz), 0; 0, 0, 1];
+Rx1 = Rx(pi - acos(- 1/sqrt(5)));
+Rz1 = Rz(2*pi/5); Rz2 = Rz(pi/5);
+
+prop = {'EdgeAlpha',.05, 'EdgeColor','none', ...
+        'FaceColor','interp', 'CData',H2C(h, CList)};
+surf(x, y,   h, prop{:})
+surf(x, y, - h, prop{:})
+[U, V, W] = matRotate(x, y, h, Rx1);
+for k = 1:5, [U, V, W] = matRotate(U, V, W, Rz1); surf(U, V,   W, prop{:}), end
+[U, V, W] = matRotate(U, V, W, Rz2);
+for k = 1:5, [U, V, W] = matRotate(U, V, W, Rz1); surf(U, V, - W, prop{:}), end
+
+    function C = H2C(H, cL)
+        X = rescale(H, 0, 1);
+        C = interp1(rescale(1:size(cL, 1), 0, 1), cL, X);
+    end
+
+    function [U, V, W] = matRotate(X, Y, Z, R)
+        U = X; V = Y; W = Z;
+        for i = 1:numel(X)
+            v = [X(i); Y(i); Z(i)];
+            n = R*v; U(i) = n(1); V(i) = n(2); W(i) = n(3);
+        end
+    end
+
+end
